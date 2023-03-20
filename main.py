f8b64bc8ec8bb8519486022c309bd3b41ee5ac57
Enes Kılıçarslan <enes.kilicarslan@hotmail.com>
2023-03-20 22:18:26 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from output import Ui_MainWindow
 import sys
 
-from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 app = QApplication(sys.argv)
