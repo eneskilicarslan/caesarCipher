63a80e5905b9b8f8f92d000e855e5d77fc101eae
Enes Kılıçarslan <enes.kilicarslan@hotmail.com>
2023-03-26 12:49:12 +0300

    add analyze attribute

--- funcs.py
+++ funcs.py
@@ -1,14 +1,16 @@
 import collections as col
+from collections import Counter
 import matplotlib.pyplot as plt
 
 
 def caesarCipher(textToCipher, key):
-
-    """ CAESAR CIPHERING
-    textToCipher: a simple text to be ciphered
-    key: variable to key through alphabet
     """
-
+    CAESAR CIPHERING
+    :param textToCipher: a simple text to be ciphered
+    :param key: variable to shift through the alphabet
+    :return: ciphered text using key
+    """
+    # TODO: number ciphering
     cipheredText = ""
     textToCipher.lower()
 
@@ -21,4 +23,26 @@ def caesarCipher(textToCipher, key):
     return cipheredText
 
 
+def freqAnalysis(textToAnalyze, path):
+    """
+    letter frequency analysis
+    :param textToAnalyze: freq analysis will be done over this var
+    :return: sorted freq dictionary
+    """
 
+    textToAnalyze.lower()
+    letterCounts = Counter(textToAnalyze.rstrip())
+    sortedLetterCounts = col.OrderedDict(sorted(letterCounts.items(), key=lambda t: t[1], reverse=True))
+    toGraph(sortedLetterCounts).savefig(path, bbox_inches='tight', transparent=True)
+
+    return sortedLetterCounts
+
+
+def toGraph(sortedLetterCounts):
+    plt.clf()
+    plt.bar(list(sortedLetterCounts.keys()), sortedLetterCounts.values(), color='black')
+    plt.xlabel("karakter")
+    plt.ylabel("frekans")
+    ax = plt.gca()
+    ax.set_facecolor('#F0F0F0')
+    return plt
